4ec549c61cc4a9b90420632e41741b5f1a1a65c8
tuxikus <contact@tuxikus.de>
2024-12-21 00:24:48 +0100

    Fix

--- modules/home-manager/app/tuxikus-emacs/lisp/config-consult.el
+++ modules/home-manager/app/tuxikus-emacs/lisp/config-consult.el
@@ -3,7 +3,7 @@
 (use-package consult
   :bind (("C-c b s" . consult-buffer)
 	 ("C-c l s" . consult-line)
-	 ("C-c r g" . consult-ripgrep))
+	 ("C-c r g" . consult-ripgrep)))
 
 (provide 'config-consult)
 
