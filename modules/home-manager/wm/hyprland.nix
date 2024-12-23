51a2c14963a43fe96bf2a480b90b0f96a0a73d26
tuxikus <contact@tuxikus.de>
2024-12-24 00:36:11 +0100

    Added window rule for ymir in hyprland

--- modules/home-manager/wm/hyprland.nix
+++ modules/home-manager/wm/hyprland.nix
@@ -135,6 +135,8 @@ in
         # Move/resize windows with mainMod + LMB/RMB and dragging
         bindm = $mainMod, mouse:272, movewindow
         bindm = $mainMod, mouse:273, resizewindow
+
+        windowrule = float, ymir
       '';
     };
 
