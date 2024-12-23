a62dd0c28a215923572f1c3b2ca6b69222b85aa0
tuxikus <contact@tuxikus.de>
2024-12-23 21:43:05 +0100

    Changes on emacs ymir functions

--- modules/home-manager/app/tuxikus-emacs/lisp/tuxikus/ymir.el
+++ modules/home-manager/app/tuxikus-emacs/lisp/tuxikus/ymir.el
@@ -1,3 +1,11 @@
-(defun tuxikus/ymir (app)
-  (interactive "sApp: ")
-  (shell-command (concat app " > /dev/null 2>&1 & disown") nil nil))
+(defun tuxikus/ymir ()
+  (interactive)
+  (let* ((applications (tuxikus/get-applications))
+	 (selected-application (completing-read "App: " applications)))
+    (shell-command (concat selected-application " > /dev/null 2>&1 & disown") nil nil)))
+
+(defun tuxikus/get-applications ()
+  (setq applications
+	(mapcar (lambda (item)
+		  (replace-regexp-in-string ".desktop" "" item))
+		(directory-files "/run/current-system/sw/share/applications"))))
