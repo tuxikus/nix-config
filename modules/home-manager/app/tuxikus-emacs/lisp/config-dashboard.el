77925a95b2e9c954092183b015f0ea577ccaf322
tuxikus <contact@tuxikus.de>
2024-12-23 15:34:16 +0100

    Updated emacs dashboard config

--- modules/home-manager/app/tuxikus-emacs/lisp/config-dashboard.el
+++ modules/home-manager/app/tuxikus-emacs/lisp/config-dashboard.el
@@ -3,7 +3,19 @@
 (use-package dashboard
   :ensure t
   :config
-  (dashboard-setup-startup-hook))
+  (dashboard-setup-startup-hook)
+
+  (setq dashboard-items '((recents   . 5)
+                        (bookmarks . 5)
+                        (projects  . 5)
+                        (agenda    . 5)
+                        (registers . 5)))
+
+  (setq dashboard-item-shortcuts '((recents   . "r")
+                                 (bookmarks . "m")
+                                 (projects  . "p")
+                                 (agenda    . "a")
+                                 (registers . "e"))))
 
 (provide 'config-dashboard)
 
