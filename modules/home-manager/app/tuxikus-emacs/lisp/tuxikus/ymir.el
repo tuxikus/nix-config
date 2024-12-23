3cf5704fc689da16f47be2df8175ef2dd74525a6
tuxikus <contact@tuxikus.de>
2024-12-23 23:37:29 +0100

    ymir use xdg-open for desktop files

--- modules/home-manager/app/tuxikus-emacs/lisp/tuxikus/ymir.el
+++ modules/home-manager/app/tuxikus-emacs/lisp/tuxikus/ymir.el
@@ -2,10 +2,7 @@
   (interactive)
   (let* ((applications (tuxikus/get-applications))
 	 (selected-application (completing-read "App: " applications)))
-    (shell-command (concat selected-application " > /dev/null 2>&1 & disown") nil nil)))
+    (shell-command (format "xdg-open %s" selected-application) nil nil)))
 
 (defun tuxikus/get-applications ()
-  (setq applications
-	(mapcar (lambda (item)
-		  (replace-regexp-in-string ".desktop" "" item))
-		(directory-files "/run/current-system/sw/share/applications"))))
+  (directory-files "/run/current-system/sw/share/applications"))
