5115e7ab40880cedee72afbc665e3df03f56c349
tuxikus <contact@tuxikus.de>
2024-12-24 17:27:15 +0100

    Fix

--- modules/home-manager/app/tuxikus-emacs/lisp/tuxikus/ymir.el
+++
@@ -1,8 +0,0 @@
-(defun tuxikus/ymir ()
-  (interactive)
-  (let* ((applications (tuxikus/get-applications))
-	 (selected-application (completing-read "App: " applications)))
-    (shell-command (format "xdg-open /run/current-system/sw/share/applications/%s" selected-application) nil nil)))
-
-(defun tuxikus/get-applications ()
-  (directory-files "/run/current-system/sw/share/applications"))
