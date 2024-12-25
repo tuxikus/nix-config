410b74c9beb894eb4dab4913a84906d18ce3b150
tuxikus <contact@tuxikus.de>
2024-12-25 20:54:45 +0100

    Test

--- hosts/zeus/disks.nix
+++ hosts/zeus/disks.nix
@@ -3,7 +3,8 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/nvme0n1";
+#        device = "/dev/nvme0n1";
+        device = "/dev/vda";
         content = {
           type = "gpt";
           partitions = {
