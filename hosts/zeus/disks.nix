535b0f14432f487b18170747ea9da6a04e019068
tuxikus <contact@tuxikus.de>
2024-12-25 23:38:28 +0100

    Fix

--- hosts/zeus/disks.nix
+++ hosts/zeus/disks.nix
@@ -8,7 +8,7 @@
           type = "gpt";
           partitions = {
             ESP = {
-              size = "512sM";
+              size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
