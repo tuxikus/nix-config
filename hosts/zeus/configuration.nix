833e397e9c62159156e44f251ba3c7a18d3329b0
tuxikus <contact@tuxikus.de>
2024-12-25 02:25:14 +0100

    Simplified config of zeus host

--- hosts/zeus/configuration.nix
+++ hosts/zeus/configuration.nix
@@ -17,217 +17,131 @@ in
     (nixModulesDirectory + "/podman.nix")
   ];
 
-  # enable nix flakes
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
 
-  # no need no to change nixos-config path if using flakes :)
-  #nix.nixPath = [
-    #  "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
-    #  "nixos-config=/home/tuxikus/.nix-config/hosts/zeus/configuration.nix"
-    #  "/nix/var/nix/profiles/per-user/root/channels"
-    #];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-    # Bootloader.
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "zeus";
+  networking.networkmanager.enable = true;
 
-    networking.hostName = "zeus"; # Define your hostname.
-    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  services.dbus.enable = true;
 
-    # Enable networking
-    networking.networkmanager.enable = true;
+  time.timeZone = "Europe/Berlin";
 
-    services.dbus.enable = true;
+  i18n.defaultLocale = "en_US.UTF-8";
 
-    # Set your time zone.
-    time.timeZone = "Europe/Berlin";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
 
-    # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
 
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "de_DE.UTF-8";
-      LC_IDENTIFICATION = "de_DE.UTF-8";
-      LC_MEASUREMENT = "de_DE.UTF-8";
-      LC_MONETARY = "de_DE.UTF-8";
-      LC_NAME = "de_DE.UTF-8";
-      LC_NUMERIC = "de_DE.UTF-8";
-      LC_PAPER = "de_DE.UTF-8";
-      LC_TELEPHONE = "de_DE.UTF-8";
-      LC_TIME = "de_DE.UTF-8";
-    };
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
-    # Enable the X11 windowing system.
-    services.xserver.enable = true;
 
-    # Enable the GNOME Desktop Environment.
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
 
-    # remove gnome default software
-    environment.gnome.excludePackages = with pkgs; [
-      orca
-      evince
-      # file-roller
-      geary
-      # gnome-disk-utility
-      # seahorse
-      # sushi
-      # sysprof
-      # adwaita-icon-theme
-      # nixos-background-info
-      # gnome-backgrounds
-      # gnome-bluetooth
-      # gnome-color-manager
-      # gnome-control-center
-      # gnome-shell-extensions
-      gnome-tour # GNOME Shell detects the .desktop file on first log-in.
-      gnome-user-docs
-      # glib # for gsettings program
-      # gnome-menus
-      # gtk3.out # for gtk-launch program
-      # xdg-user-dirs # Update user dirs as described in https://freedesktop.org/wiki/Software/xdg-user-dirs/
-      # xdg-user-dirs-gtk # Used to create the default bookmarks
-      #
-      baobab
-      epiphany
-      gnome-text-editor
-      gnome-calculator
-      #gnome-calendar
-      gnome-characters
-      # gnome-clocks
-      gnome-console
-      gnome-contacts
-      gnome-font-viewer
-      gnome-logs
-      gnome-maps
-      gnome-music
-      # gnome-system-monitor
-      gnome-weather
-      loupe
-      # nautilus
-      gnome-connections
-      simple-scan
-      snapshot
-      totem
-      yelp
-      gnome-software
+  # Enable CUPS to print documents.
+  #services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  #######################################
+  ### Groups
+  # group of main user
+  users.groups.tuxikus = { };
+  # group to flash zsa moonlander mk1 keyboard with oryx in chromium
+  users.groups.plugdev = { };
+  #######################################
+  ### Users
+  # main user
+  users.users.tuxikus = {
+    isNormalUser = true;
+    description = "tuxikus";
+    group = "tuxikus";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "plugdev"
     ];
+  };
+  ########################################
 
+  fonts.packages = with pkgs; [
+    nerdfonts
+  ];
 
-    # Configure keymap in X11
-    services.xserver.xkb = {
-      layout = "us";
-      variant = "";
-    };
+  environment.systemPackages = with pkgs; [
+    firefox
+    chromium
+    home-manager
+    pavucontrol
+    python3
+    foot
+    wget
+    hyprpaper
+    dunst
+    waybar
+    grim
+    slurp
+    bat
+    ripgrep
+    fzf
+    btop
+    fastfetch
+    keepassxc
+    unzip
+    mpv
+    dex
+  ];
 
-    # Enable CUPS to print documents.
-    #services.printing.enable = true;
+  # hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
 
-    # Enable sound with pipewire.
-    hardware.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
+  # ssh
+  programs.ssh.startAgent = true;
 
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
-    };
-
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
-
-    #######################################
-    ### Groups
-    # group of main user
-    users.groups.tuxikus = { };
-    # group to flash zsa moonlander mk1 keyboard with oryx in chromium
-    users.groups.plugdev = { };
-    #######################################
-    ### Users
-    # main user
-    users.users.tuxikus = {
-      isNormalUser = true;
-      description = "tuxikus";
-      group = "tuxikus";
-      extraGroups = [
-        "networkmanager"
-        "wheel"
-        "plugdev"
-      ];
-    };
-    ########################################
-
-    fonts.packages = with pkgs; [
-      nerdfonts
-    ];
-
-    environment.systemPackages = with pkgs; [
-      firefox
-      chromium
-      home-manager
-      pavucontrol
-      python3
-      foot
-      wget
-      hyprpaper
-      dunst
-      waybar
-      grim
-      slurp
-      bat
-      ripgrep
-      fzf
-      btop
-      fastfetch
-      keepassxc
-      unzip
-      mpv
-      dex
-    ];
-
-    # hyprland
-    programs.hyprland = {
-      enable = true;
-      xwayland.enable = true;
-    };
-
-    # ssh
-    programs.ssh.startAgent = true;
-
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-      #   enable = true;
-      #   enableSSHSupport = true;
-      # };
-
-      # List services that you want to enable:
-
-      # Enable the OpenSSH daemon.
-      # services.openssh.enable = true;
-
-      # Open ports in the firewall.
-      # networking.firewall.allowedTCPPorts = [ ... ];
-      # networking.firewall.allowedUDPPorts = [ ... ];
-      # Or disable the firewall altogether.
-      # networking.firewall.enable = false;
-
-      # This value determines the NixOS release from which the default
-      # settings for stateful data, like file locations and database versions
-      # on your system were taken. It‘s perfectly fine and recommended to leave
-      # this value at the release version of the first install of this system.
-      # Before changing this value read the documentation for this option
-      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-      system.stateVersion = "24.05"; # Did you read the comment?
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
