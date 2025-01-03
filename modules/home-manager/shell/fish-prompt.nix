d102842a48e3bf8f680a5a68b1b4506740a5893d
tuxikus <contact@tuxikus.de>
2025-01-03 11:58:02 +0100

    Fix

--- modules/home-manager/shell/fish-prompt.nix
+++ modules/home-manager/shell/fish-prompt.nix
@@ -1,5 +1,5 @@
 {
-  home.file.".config/fish/functions/fish_prompt" = {
+  home.file.".config/fish/functions/fish_prompt.fish" = {
     text = ''
       function fish_prompt
         printf '%s %s%s%s \n > ' "[$status]" (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
