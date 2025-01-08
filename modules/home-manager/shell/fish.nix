66fc86321729e5bb67a5eabcba24f5199f1ce0ce
tuxikus <contact@tuxikus.de>
2025-01-08 22:20:38 +0100

    Fix

--- modules/home-manager/shell/fish.nix
+++ modules/home-manager/shell/fish.nix
@@ -13,10 +13,6 @@
       function fish_prompt
         printf '%s %s%s%s \n > ' "[$status]" (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
       end
-
-      function fish_right_prompt
-        printf '%s' 🐟
-      end
     '';
   };
 }
