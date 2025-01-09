cd3a0ebebc98f51b03cfddbd0c1ceb5312cfb41a
tuxikus <contact@tuxikus.de>
2025-01-09 21:05:24 +0100

    Added nix gc

--- hosts/zeus/configuration.nix
+++ hosts/zeus/configuration.nix
@@ -165,6 +165,12 @@ in
   # ssh
   programs.ssh.startAgent = true;
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
