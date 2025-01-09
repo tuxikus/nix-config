6742ea2d95d65eebf1ebd2b6e150f57b5f638599
tuxikus <contact@tuxikus.de>
2025-01-09 20:59:00 +0100

    Test

--- modules/home-manager/app/tuxikus-emacs/lisp/init-all-the-icons.el
+++ modules/home-manager/app/tuxikus-emacs/lisp/init-all-the-icons.el
@@ -3,6 +3,13 @@
 (use-package all-the-icons
   :if (display-graphic-p))
 
+(define-minor-mode all-the-icons-completion-mode
+  "Add icons to completion candidates."
+  :global t
+  (if all-the-icons-completion-mode
+      (advice-add (compat-function completion-metadata-get) :around #'all-the-icons-completion-completion-metadata-get)
+    (advice-remove (compat-function completion-metadata-get) #'all-the-icons-completion-completion-metadata-get)))
+
 (use-package all-the-icons-completion
       :after (marginalia all-the-icons)
       :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
