faf46c95691fd781173a16371de63bf24694680d
tuxikus <contact@tuxikus.de>
2025-01-09 20:48:16 +0100

    Revert

--- modules/home-manager/app/tuxikus-emacs/lisp/init-dired.el
+++ modules/home-manager/app/tuxikus-emacs/lisp/init-dired.el
@@ -1,7 +1,7 @@
 ;; init-dired.el --- -*- lexical-binding: t -*-
 
 ;; less verbose
-;;(add-hook 'dired-mode-hook (lambda () (dired-hide-details-mode 1)))
+(add-hook 'dired-mode-hook (lambda () (dired-hide-details-mode 1)))
 
 ;; enable a
 (put 'dired-find-alternate-file 'disabled nil)
