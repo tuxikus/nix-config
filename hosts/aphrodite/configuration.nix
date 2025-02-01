e7d4864e4013115d732291f964cdd61ecb2f25f6
tuxikus <contact@tuxikus.de>
2025-02-01 19:54:24 +0100

    Changes

--- hosts/aphrodite/configuration.nix
+++ hosts/aphrodite/configuration.nix
@@ -21,6 +21,7 @@
     pkgs.openssh
     pkgs.jupyter
     pkgs.tree-sitter
+    pkgs.poppler_utils
 
     pkgs.fishPlugins.fzf-fish
     pkgs.fishPlugins.puffer
@@ -47,10 +48,72 @@
   programs.bash.enable = true;
 
   services.nix-daemon.enable = true;
-  services.emacs.enable = true;
-  services.aerospace.enable = true;
+
+  services.aerospace = {
+    enable = true;
+    settings = {
+      gaps = {
+        inner.horizontal = 22;
+        inner.vertical = 22;
+        outer.left = 15;
+        outer.bottom = 15;
+        #outer.top = [{ monitor."T34w-30" = 50 }];
+        outer.top = 15;
+        outer.right = 15;
+      };
+      mode.main.binding = {
+        cmd-left = "focus left";
+        cmd-down = "focus down";
+        cmd-up = "focus up";
+        cmd-right = "focus right";
+
+        cmd-shift-left = "move left";
+        cmd-shift-down = "move down";
+        cmd-shift-up = "move up";
+        cmd-shift-right = "move right";
+
+        cmd-m = "fullscreen";
+
+        cmd-1 = "workspace 1";
+        cmd-2 = "workspace 2";
+        cmd-3 = "workspace 3";
+        cmd-4 = "workspace 4";
+        cmd-5 = "workspace 5";
+        cmd-6 = "workspace 6";
+        cmd-7 = "workspace 7";
+        cmd-8 = "workspace 8";
+        cmd-9 = "workspace 9";
+        cmd-0 = "workspace 10";
+        
+        cmd-shift-1 = "move-node-to-workspace 1";
+        cmd-shift-2 = "move-node-to-workspace 2";
+        cmd-shift-3 = "move-node-to-workspace 3";
+        cmd-shift-4 = "move-node-to-workspace 4";
+        cmd-shift-5 = "move-node-to-workspace 5";
+        cmd-shift-6 = "move-node-to-workspace 6";
+        cmd-shift-7 = "move-node-to-workspace 7";
+        cmd-shift-8 = "move-node-to-workspace 8";
+        cmd-shift-9 = "move-node-to-workspace 9";
+        cmd-shift-0 = "move-node-to-workspace 10";
+
+        cmd-r = "mode resize";
+      };
+      mode.resize.binding = {
+        h = "resize width -50";
+        j = "resize height +50";
+        k = "resize height -50";
+        l = "resize width +50";
+        enter = "mode main";
+        esc = "mode main";
+      };
+    };
+  };
   services.sketchybar.enable = true;
-  services.jankyborders.enable = true;
+  services.jankyborders = {
+    enable = true;
+    active_color = "0xFFFF0000";
+    width = 7.0;
+  };
   #services.karabiner-elements.enable = true;
 
   nix.settings.experimental-features = "nix-command flakes";
