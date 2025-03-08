7ea24b958a5301d18d174289fe03550d098cf6e9
tuxikus <contact@tuxikus.de>
2025-03-08 19:13:31 +0100

    Added bash aliases for emacs

--- modules/home-manager/shell/bash.nix
+++ modules/home-manager/shell/bash.nix
@@ -5,10 +5,12 @@
     enableCompletion = true;
     initExtra = "PS1='[$?] \\w \\$ '";
     shellAliases = {
-	night-shift-on = "hyprsunset --temperature 3000 & disown";
-	night-shift-off = "pgrep hyprsunset | xargs kill";
-	ll = "ls -lah";
-	ff = "fastfetch";
+      ed = "emacs --daemon";
+      e = "emacsclient -c";
+	    night-shift-on = "hyprsunset --temperature 3000 & disown";
+	    night-shift-off = "pgrep hyprsunset | xargs kill";
+	    ll = "ls -lah";
+	    ff = "fastfetch";
     };
   };
 }
