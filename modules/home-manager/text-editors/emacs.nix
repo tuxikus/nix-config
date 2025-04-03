bf207bd582544a17b5da62daa182c50d5c2c093c
tuxikus <contact@tuxikus.de>
2025-04-03 20:20:28 +0200

    Emacs config changes

--- modules/home-manager/text-editors/emacs.nix
+++ modules/home-manager/text-editors/emacs.nix
@@ -24,6 +24,7 @@ let
       dired-rsync
       dired-rsync-transient
       direnv
+      dirvish
       docker
       htmlize
       doom-modeline
@@ -117,7 +118,8 @@ in
       ("C-S-e" . end-of-buffer)
     
       :hook
-      (before-save . whitespace-cleanup)
+      ((before-save . whitespace-cleanup)
+      (makefile-mode . indent-tabs-mode))
     
       :init
       (fset 'yes-or-no-p 'y-or-n-p)
@@ -133,26 +135,11 @@ in
       ;;(setq-default cursor-type 'box)
     
       ;; line numbers
-      ;; (setq display-line-numbers-type 'relative)
-      ;; (global-display-line-numbers-mode)
+      (setq display-line-numbers-type 'relative)
+      (global-display-line-numbers-mode)
     
       (load-theme 'doom-bluloco-light t)
     
-      ;; Add prompt indicator to `completing-read-multiple'.
-      ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
-      (defun crm-indicator (args)
-        (cons (format "[CRM%s] %s"
-                            (replace-regexp-in-string
-                             "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
-                             crm-separator)
-                            (car args))
-                    (cdr args)))
-      (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
-    
-      ;; Do not allow the cursor in the minibuffer prompt
-      (setq minibuffer-prompt-properties
-                  '(read-only t cursor-intangible t face minibuffer-prompt))
-      (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
       :config
       (setq create-lockfiles nil
           make-backup-files nil
@@ -165,13 +152,12 @@ in
     
       (add-to-list 'default-frame-alist
                    '(font . "Iosevka Nerd Font-${config.fontSize}"))
+    
       (which-key-mode 1)
     
       :custom
       (enable-recursive-minibuffers t)
       (read-extended-command-predicate #'command-completion-default-include-p)
-    
-      ;; (tab-always-indent 'complete)
     
       ;; Emacs 30 and newer: Disable Ispell completion function.
       ;; Try `cape-dict' as an alternative.
@@ -182,12 +168,17 @@ in
       ;; useful beyond Corfu.
       (read-extended-command-predicate #'command-completion-default-include-p))
     
+    (use-package dirvish
+      :init
+      (dirvish-override-dired-mode))
+    
     (use-package aas
-      :hook (LaTeX-mode . aas-activate-for-major-mode)
-      :hook (org-mode . aas-activate-for-major-mode)
+      :hook
+      ((LaTeX-mode . aas-activate-for-major-mode)
+       (org-mode . aas-activate-for-major-mode))
+    
       :config
       (aas-set-snippets 'text-mode
-        ;; expand unconditionally
         "oe" "ö"
         "Oe" "Ö"
         "ue" "ü"
@@ -199,6 +190,7 @@ in
     (use-package ace-window
       :bind
       (("M-o" . ace-window))
+    
       :init
       (setq aw-dispatch-always t)
       (setq aw-keys '(?a ?o ?e ?u ?h ?t ?n ?s ?f)))
@@ -230,33 +222,25 @@ in
        ("C-c k" . consult-kmacro)
        ("C-c m" . consult-man)
        ("C-c i" . consult-info)
-       ([remap Info-search] . consult-info)
-       ;; C-x bindings in `ctl-x-map'
-       ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
-       ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
-       ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-       ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-       ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
-       ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
-       ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
-       ;; Custom M-# bindings for fast register access
+       ("C-x b" . consult-buffer)
+       ("C-x 4 b" . consult-buffer-other-window)
+       ("C-x 5 b" . consult-buffer-other-frame)
+       ("C-x t b" . consult-buffer-other-tab)
+       ("C-x r b" . consult-bookmark)
+       ("C-x p b" . consult-project-buffer)
        ("M-#" . consult-register-load)
-       ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+       ("M-'" . consult-register-store)
        ("C-M-#" . consult-register)
-       ;; Other custom bindings
-       ("M-y" . consult-yank-pop)                ;; orig. yank-pop
-       ;; M-g bindings in `goto-map'
+       ("M-y" . consult-yank-pop)
        ("M-g e" . consult-compile-error)
-       ;;("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
-       ("M-g g" . consult-goto-line)             ;; orig. goto-line
-       ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
-       ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+       ("M-g g" . consult-goto-line)
+       ("M-g M-g" . consult-goto-line)
+       ("M-g o" . consult-outline)
        ("M-g m" . consult-mark)
        ("M-g k" . consult-global-mark)
        ("M-g i" . consult-imenu)
        ("M-g I" . consult-imenu-multi)
-       ;; M-s bindings in `search-map'
-       ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+       ("M-s d" . consult-find)
        ("M-s c" . consult-locate)
        ("M-s g" . consult-grep)
        ("M-s G" . consult-git-grep)
@@ -265,17 +249,7 @@ in
        ("M-s L" . consult-line-multi)
        ("M-s k" . consult-keep-lines)
        ("M-s u" . consult-focus-lines)
-       ;; Isearch integration
-       ("M-s e" . consult-isearch-history)
-       :map isearch-mode-map
-       ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
-       ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
-       ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
-       ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
-       ;; Minibuffer history
-       :map minibuffer-local-map
-       ("M-s" . consult-history)                 ;; orig. next-matching-history-element
-       ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+    
       :config
       (consult-customize consult--source-buffer :hidden t :default nil)
       (add-to-list 'consult-buffer-sources persp-consult-source))
@@ -312,12 +286,12 @@ in
     
       (dashboard-setup-startup-hook))
     
-    (use-package dired
-      :config
-      (put 'dired-find-alternate-file 'disabled nil))
+    ;; (use-package dired
+    ;;   :config
+    ;;   (put 'dired-find-alternate-file 'disabled nil))
     
     (use-package direnv
-      :config
+      :init
       (direnv-mode))
     
     (use-package pdf-tools
