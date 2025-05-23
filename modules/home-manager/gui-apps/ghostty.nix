1d9f2711c2712297257b091861e0c3d877151f23
tuxikus <contact@tuxikus.de>
2025-05-23 11:27:01 +0200

    ghostty key changes

--- modules/home-manager/gui-apps/ghostty.nix
+++ modules/home-manager/gui-apps/ghostty.nix
@@ -14,6 +14,15 @@
       confirm-close-surface = false
       
       theme = BlulocoLight
+      
+      # emacs bindings
+      keybind = ctrl+v=scroll_page_down
+      keybind = alt+v=scroll_page_up
+      
+      keybind = ctrl+q=toggle_tab_overview
+      
+      keybind = ctrl+x>3=new_split:right
+      keybind = ctrl+x>2=new_split:down
     '';
   };
 }
