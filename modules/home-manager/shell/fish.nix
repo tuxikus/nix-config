e3e1e663e90e30855e588ae52eb90bf44a63d1f4
tuxikus <contact@tuxikus.de>
2025-06-20 12:21:47 +0200

    fish shell changes

--- modules/home-manager/shell/fish.nix
+++ modules/home-manager/shell/fish.nix
@@ -3,6 +3,22 @@
     enable = true;
     interactiveShellInit = ''
       set -g fish_greeting ""
+      set -g fish_color_quote green
+      set -g fish_color_param magenta
     '';
+    functions = {
+      fish_prompt = {
+        body = ''
+          set -l last_status $status
+          set -l stat
+          set stat "[$last_status]"
+          string join ' ' -- $stat (prompt_pwd --full-length-dirs 4)
+          echo -e '$ '
+        '';
+      };
+      fish_right_prompt = {
+        body = "date '+%H:%M'";
+      };
+    };
   };
 }
